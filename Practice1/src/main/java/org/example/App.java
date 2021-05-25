dfaa6503976774f6d7bc184d9150941091b4e02a
Nikita Maslov <maslov.n.e@gmail.com>
2021-05-25 21:21:52 +0300

    finalize practice1

---
+++ Practice1/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) throws Exception {
+        System.out.println("Practice 1");
+    }
+}
